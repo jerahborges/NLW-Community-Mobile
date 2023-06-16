43dc3b9788e156bc85f21a782ab410524ebd7712
Jerah <jerah_borges@hotmail.com>
2023-06-16 11:55:18 -0300

    FirstCommit

---
+++ App.tsx
@@ -0,0 +1,64 @@
+import { StatusBar } from 'expo-status-bar'
+import { ImageBackground, View, Text, TouchableOpacity } from 'react-native'
+import BlurBg from './src/assets/luz-blur.png'
+import Stripes from './src/assets/stripes.svg'
+import NLWLogo from './src/assets/nlw-spaceTime-logo.svg'
+
+import {
+  useFonts,
+  Roboto_400Regular,
+  Roboto_700Bold,
+} from '@expo-google-fonts/roboto'
+
+import { BaiJamjuree_700Bold } from '@expo-google-fonts/bai-jamjuree'
+import { styled } from 'nativewind'
+
+const StulesStripes = styled(Stripes)
+
+export default function App() {
+  const [hasLoadedFonts] = useFonts({
+    Roboto_400Regular,
+    Roboto_700Bold,
+    BaiJamjuree_700Bold,
+  })
+
+  if (!hasLoadedFonts) {
+    return null
+  }
+  return (
+    <ImageBackground
+      source={BlurBg}
+      className="relative flex-1 bg-gray-900 px-8 py-10"
+      imageStyle={{ position: 'absolute', left: '-100%' }}
+    >
+      <StulesStripes className="absolute left-2" />
+
+      <View className="flex-1 items-center justify-center gap-6">
+        <NLWLogo />
+
+        <View className="space-y-2">
+          <Text className="text-center font-title text-2xl leading-tight text-gray-50">
+            Sua Cápsula do tempo
+          </Text>
+          <Text className="text-center font-body text-base leading-relaxed text-gray-100">
+            Colecione momentos marcantes da sua jornada e compartilhe (se
+            quiser) com o mundo!
+          </Text>
+        </View>
+
+        <TouchableOpacity
+          activeOpacity={0.7}
+          className="rounded-full bg-green-500 px-5 py-3"
+        >
+          <Text className="font-alt text-sm uppercase text-black">
+            Cadastrar lembrança
+          </Text>
+        </TouchableOpacity>
+      </View>
+      <Text className="text-center font-body text-sm leading-relaxed text-gray-200">
+        Feito com 💜 no NLW da Rocketseat.
+      </Text>
+      <StatusBar style="light" translucent />
+    </ImageBackground>
+  )
+}
